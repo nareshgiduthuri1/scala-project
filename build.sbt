30d757eb72eab4b0e73edb09df4b64d0b0cb5e70
naresh giduthuri <nareshgiduthuri1@gmail.com>
2021-05-29 21:55:26 +0530

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "scala project"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,73 @@
+object Hello {
+  def main(args: Array[String]) = {
+    /*val name = "Ash"
+    val age = 18
+    println("Hello "+ name +" your age is "+age)
+    println(s"$name is $age years old") //string interpolation
+    println(f"$name%s is $age%d years old") //f string interpolation
+    println(s"Hello \nWorld")
+    println(raw"Hello \nWorld")
+
+    // iF-eLSE sTATEMENTS
+
+    val x = 21;
+    val y = 20;
+    var res = "";
+
+    if (x == 21 && y == 20){
+      res = "x is 20";
+    } else {
+      res = "x != 20";
+    }
+    println(res);
+
+    val res2 = if (x !=20) "x == 20" else "x !=20";
+    println(res2)
+    println(if (x !=20) "x == 20" else "x !=20");
+
+    // While & Do-While Loops
+    var x = 0;
+
+    while (x<10){
+      println("x = "+x)
+      x+=1;
+    }
+
+    var y = 0;
+    do {
+      println("y is "+y)
+      y+=1;
+    } while (y<10);
+
+    // For Loops
+
+    for (i <- 1.to(5)){  // OR for (i <- 1 to 5){
+      println("i is using "+ i);
+    }
+    for (i <- 1.until(6)){  // OR for (i <- 1 until 6){
+      println("i is using until "+ i);
+    }
+    for (i <- 1.until(6); j<- 1 to 3){  // OR for (i <- 1 until 6){
+      println("i is using until "+ i + " " +j);
+    }
+
+    val lst = List(1,2,3,4,5,6,7,8,9,10)
+
+    for (i <- lst){
+      println(" From list "+i)
+    }
+    for (i <- lst; if i>6){
+      println(" From filtered list "+i)
+    }
+
+    val result = for {i <- lst; if i>6} yield {
+      i * i
+    }
+    println("result is "+result); */
+
+    // Matched Expressions
+
+
+
+  }
+}
